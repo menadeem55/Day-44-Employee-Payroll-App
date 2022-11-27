c64487a77810161c0f39eefd4cd9b3dc21ef01e1
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-27 22:11:46 +0530

    [Added: Payroll Object]

--- employee.js
+++ employee.js
@@ -13,7 +13,6 @@ window.addEventListener('DOMContentLoaded', (event) =>{
         textError.textContent = e;
     }
 });
-
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
